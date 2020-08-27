99ced160e4a64f677912b109dedfaaf4b24e41a4
Teddy Koker <teddy.koker@gmail.com>
2020-08-27 18:04:39 -0400

    comments

--- components.py
+++ components.py
@@ -1,5 +1,12 @@
 from torch import nn
 
+"""
+Simple stacked conv/convtranspose encoder/decoder for proof of concept.
+
+In addition to linear output for mu and logvar, encoder now outputs
+classification logits to be used for fine-tuning.
+"""
+
 
 def _conv(in_channels, out_channels):
     return nn.Sequential(
@@ -20,16 +27,17 @@ def _deconv(in_channels, out_channels, non_linearity=nn.ReLU):
 
 
 class Encoder(nn.Module):
-    def __init__(self, latent_dim=128, in_channels=3, max_channels=128):
+    def __init__(self, latent_dim=128, in_channels=3, max_channels=256, num_classes=10):
         super().__init__()
-        # construct encoder
         self.encoder = nn.Sequential(
-            _conv(in_channels, max_channels // 4),
+            _conv(in_channels, max_channels // 8),
+            _conv(max_channels // 8, max_channels // 4),
             _conv(max_channels // 4, max_channels // 2),
             _conv(max_channels // 2, max_channels),
         )
-        self.fc_mu = nn.Linear(max_channels * 16, latent_dim)
-        self.fc_var = nn.Linear(max_channels * 16, latent_dim)
+        self.fc_mu = nn.Linear(max_channels * 4, latent_dim)
+        self.fc_var = nn.Linear(max_channels * 4, latent_dim)
+        self.fc_clf = nn.Linear(max_channels * 4, num_classes)
 
     def forward(self, x):
         x = self.encoder(x)
@@ -37,22 +45,23 @@ class Encoder(nn.Module):
 
         mu = self.fc_mu(x)
         log_var = self.fc_var(x)
-        return mu, log_var
+        logits = self.fc_clf(x)
+        return mu, log_var, logits
 
 
 class Decoder(nn.Module):
-    def __init__(self, latent_dim=128, out_channels=3, max_channels=128):
+    def __init__(self, latent_dim=128, out_channels=3, max_channels=256):
         super().__init__()
-        # construct decoder
         self.max_channels = max_channels
-        self.decoder_in = nn.Linear(latent_dim, max_channels * 16)
+        self.decoder_in = nn.Linear(latent_dim, max_channels * 4)
         self.decoder = nn.Sequential(
             _deconv(max_channels, max_channels // 2),
             _deconv(max_channels // 2, max_channels // 4),
-            _deconv(max_channels // 4, out_channels, non_linearity=nn.Sigmoid),
+            _deconv(max_channels // 4, max_channels // 8),
+            _deconv(max_channels // 8, out_channels, non_linearity=nn.Sigmoid),
         )
 
     def forward(self, z):
         x_hat = self.decoder_in(z)
-        x_hat = x_hat.view(-1, self.max_channels, 4, 4)
+        x_hat = x_hat.view(-1, self.max_channels, 2, 2)
         return self.decoder(x_hat)
--- vae.py
+++ vae.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -6,9 +7,11 @@ from torchvision.datasets import CIFAR10
 import torchvision.transforms as T
 
 import pytorch_lightning as pl
+import pytorch_lightning.metrics.functional as FM
 from pl_bolts.datamodules import CIFAR10DataModule
 
 from components import Encoder, Decoder
+from simclr_transforms import SimCLRTrainDataTransform, SimCLREvalDataTransform
 
 
 def reparameterize(mu, log_var):
@@ -17,42 +20,69 @@ def reparameterize(mu, log_var):
     return eps * std + mu
 
 
-# def kl_divergence(mu, log_var): return torch.mean(
-#        -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0
-#    )
-
-
 def kl_divergence(mu, log_var):
     return -0.5 * torch.mean(1 + log_var - mu ** 2 - log_var.exp())
 
 
 class VAE(pl.LightningModule):
-    def __init__(self, kl_coeff: float, latent_dim=128, max_hidden=128, lr=1e-3):
+    def __init__(
+        self,
+        kl_coeff: float,
+        latent_dim=128,
+        max_hidden=256,
+        lr=1e-3,
+        finetune=False,
+        num_classes=10,
+    ):
         super(VAE, self).__init__()
         self.save_hyperparameters()
+        self.finetune = finetune
         self.kl_coeff = kl_coeff
         self.lr = lr
-        self.encode = Encoder(latent_dim, max_channels=max_hidden)
+        self.encode = Encoder(
+            latent_dim, max_channels=max_hidden, num_classes=num_classes
+        )
         self.decode = Decoder(latent_dim, max_channels=max_hidden)
 
     def forward(self, x):
-        mu, log_var = self.encode(x)
+        mu, log_var, _ = self.encode(x)
         z = reparameterize(mu, log_var)
         return z, self.decode(z), mu, log_var
 
+    def step(self, batch, batch_idx):
+        (x1, x2), y = batch
+
+        if self.finetune:
+            _, _, logits = self.encode(x1)
+            loss = F.cross_entropy(logits, y)
+            acc = FM.accuracy(logits, y)
+            return loss, {"ce_loss": loss, "acc": acc}
+
+        z1, x1_hat, mu, log_var = self.forward(x1)
+        recon = F.binary_cross_entropy(x1_hat, x1)
+        kl = kl_divergence(mu, log_var)
+        loss = recon + self.kl_coeff * kl
+
+        # TODO: experiment with cosine similarity
+        # something like
+        # if self.cosine:
+        #     z2, _, _, _ = self.forward(x2)
+        #     loss += F.cosine_similarity(z1, z2, dim=1)
+
+        return loss, {"loss": loss, "kl": kl, "recon": recon}
+
     def training_step(self, batch, batch_idx):
-        x, _ = batch
-        z, x_hat, mu, log_var = self.forward(x)
-
-        reconst_loss = F.binary_cross_entropy(x_hat, x)
-        kl_loss = kl_divergence(mu, log_var)
-
-        loss = reconst_loss + self.kl_coeff * kl_loss
-
+        loss, logs = self.step(batch, batch_idx)
         result = pl.TrainResult(minimize=loss)
-        result.log("loss", loss)
-        result.log("kl", kl_loss)
-        result.log("reconst", reconst_loss)
+        result.log_dict(
+            {f"train_{k}": v for k, v in logs.items()}, on_step=True, on_epoch=False
+        )
+        return result
+
+    def validation_step(self, batch, batch_idx):
+        loss, logs = self.step(batch, batch_idx)
+        result = pl.EvalResult(checkpoint_on=loss)
+        result.log_dict({f"val_{k}": v for k, v in logs.items()})
         return result
 
     def configure_optimizers(self):
@@ -60,19 +90,27 @@ class VAE(pl.LightningModule):
 
 
 if __name__ == "__main__":
-    batch_size = 64
-    learning_rate = 3e-4
-    max_epochs = 20
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--learning_rate", type=float, default=3e-4)
+    parser.add_argument("--max_epochs", type=int, default=200)
+    parser.add_argument("--pretrained", type=str, default=None)
+    parser.add_argument("--finetune", action="store_true")
 
-    transform = T.Compose(
-        [T.RandomCrop(32, padding=4), T.RandomHorizontalFlip(), T.ToTensor()]
-    )
+    args = parser.parse_args()
 
-    dataset = CIFAR10("data", train=True, download=True, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=8)
+    dm = CIFAR10DataModule(batch_size=args.batch_size, num_workers=6)
+    dm.train_transforms = SimCLRTrainDataTransform(input_height=32)
+    dm.test_transforms = SimCLREvalDataTransform(input_height=32)
+    dm.val_transforms = SimCLREvalDataTransform(input_height=32)
 
-    kl_coeff = batch_size / len(dataset)  # important
+    kl_coeff = args.batch_size / dm.num_samples
 
-    model = VAE(lr=learning_rate, kl_coeff=kl_coeff)
-    trainer = pl.Trainer(gpus=1, max_epochs=max_epochs)
-    trainer.fit(model, dataloader)
+    if args.pretrained is not None:
+        model = VAE.load_from_checkpoint(
+            args.pretrained, lr=args.learning_rate, finetune=args.finetune
+        )
+    model = VAE(lr=args.learning_rate, kl_coeff=kl_coeff, finetune=args.finetune)
+
+    trainer = pl.Trainer(gpus=1, max_epochs=args.max_epochs)
+    trainer.fit(model, dm)
