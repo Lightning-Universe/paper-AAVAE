c4f50e4108d0ba2d93e03315daf8a8d6df9a0251
Teddy Koker <teddy.koker@gmail.com>
2020-09-09 16:43:23 -0400

    fixed local transform

--- vae/transforms.py
+++ vae/transforms.py
@@ -11,17 +11,17 @@ class Identity:
 class GlobalTransform:
     def __init__(self, flip: bool = False, jitter_strength: float = 1.0):
         color_jitter = T.ColorJitter(
-            0.8 * self.jitter_strength,
-            0.8 * self.jitter_strength,
-            0.8 * self.jitter_strength,
-            0.2 * self.jitter_strength,
+            0.8 * jitter_strength,
+            0.8 * jitter_strength,
+            0.8 * jitter_strength,
+            0.2 * jitter_strength,
         )
 
-        transforms.append(T.RandomApply([self.color_jitter], p=0.8))
+        transforms = []
         if flip:
             transforms.append(T.RandomHorizontalFlip())
 
-        transforms.append(T.RandomApply([self.color_jitter], p=0.8))
+        transforms.append(T.RandomApply([color_jitter], p=0.8))
         transforms.append(T.RandomGrayscale(p=0.2))
         self.transforms = T.Compose(transforms)
 
@@ -31,7 +31,7 @@ class GlobalTransform:
 
 class LocalTransform:
     def __init__(self, size=32):
-        self.transforms = T.Compose(GlobalTransform(), T.RandomResizedCrop(size=size))
+        self.transforms = T.Compose([GlobalTransform(), T.RandomResizedCrop(size=size)])
 
     def __call__(self, x):
         return self.transforms(x)
