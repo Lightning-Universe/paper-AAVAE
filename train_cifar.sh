e0eb01693ffabc470764e7be05d0df1f72ce177e
Teddy Koker <teddy.koker@gmail.com>
2020-12-08 18:00:20 -0500

    update grid scripts

--- train_cifar.sh
+++ train_cifar.sh
@@ -1,2 +1,2 @@
 # train CIFAR10 on one V100 GPU, should take ~28 hours
-grid train --grid_instance p3.2xlarge src/train.py
+grid train --grid_instance_type p3.2xlarge --grid_gpus 1 src/train.py --gpus 1
--- train_stl.sh
+++ train_stl.sh
@@ -1,2 +1,2 @@
 # train STL10 on four V100 GPU
-grid train --grid_instance p3.8xlarge src/train.py --dataset stl10 --batch_size 56
+grid train --grid_instance_type p3.8xlarge src/train.py --grid_gpus 4 --dataset stl10 --batch_size 56 --gpus 4
