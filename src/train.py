ad6a8cbce3bb0c47b4658f7b60905222331fe0cb
Teddy Koker <teddy.koker@gmail.com>
2021-01-12 12:48:40 -0500

    marginal logpx fix

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ from torchvision.datasets import CIFAR10
 import torchvision.transforms as T
 import numpy as np
 from torch.optim import Adam
-from einops import repeat
+from einops import repeat, rearrange
 import math
 
 import pytorch_lightning as pl
@@ -147,7 +147,7 @@ class VAE(pl.LightningModule):
         decoder="resnet18",
         first_conv=False,
         maxpool1=False,
-        dataset='cifar10',
+        dataset="cifar10",
         max_epochs=100,
         warmup_epochs=10,
         analytic=False,
@@ -235,7 +235,7 @@ class VAE(pl.LightningModule):
         return kl, log_pz, log_qz
 
     def step(self, batch, samples=1):
-        if self.dataset == 'stl10':
+        if self.dataset == "stl10":
             unlabeled_batch = batch[0]
             batch = unlabeled_batch
 
@@ -263,9 +263,14 @@ class VAE(pl.LightningModule):
         elbo = (kl - log_pxz).mean()
         loss = (self.kl_coeff * kl - log_pxz).mean()
 
-        # marginal likelihood (batch * samples) elements here
-        log_px = torch.logsumexp(log_pxz + log_pz - log_qz, dim=0) - np.log(
-            batch_size * samples
+        # add samples dimension back
+        log_pxz = rearrange(log_pxz, "(b samples) -> b samples", samples=samples)
+        log_pz = rearrange(log_pz, "(b samples) -> b samples", samples=samples)
+        log_qz = rearrange(log_qz, "(b samples) -> b samples", samples=samples)
+
+        # marginal likelihood, logsumexp over sample dim, mean ove batch dim
+        log_px = torch.logsumexp(log_pxz + log_pz - log_qz, dim=1).mean(dim=0) - np.log(
+            samples
         )
         bpd = -log_px / (pixels * np.log(2))  # need log_px in base 2
 
@@ -309,7 +314,7 @@ class VAE(pl.LightningModule):
                 ),
             ),
             "interval": "step",
-            'frequency': 1,
+            "frequency": 1,
         }
 
         return [optimizer], [scheduler]
@@ -323,50 +328,50 @@ if __name__ == "__main__":
     # encoder/decoder params
     parser.add_argument("--encoder", default="resnet50", choices=encoders.keys())
     parser.add_argument("--decoder", default="resnet50", choices=decoders.keys())
-    parser.add_argument('--h_dim', type=int, default=2048)
-    parser.add_argument('--first_conv', type=bool, default=True)
-    parser.add_argument('--maxpool1', type=bool, default=True)
+    parser.add_argument("--h_dim", type=int, default=2048)
+    parser.add_argument("--first_conv", type=bool, default=True)
+    parser.add_argument("--maxpool1", type=bool, default=True)
 
     # vae params
-    parser.add_argument('--kl_coeff', type=float, default=0.1)
-    parser.add_argument('--latent_dim', type=int, default=128)
+    parser.add_argument("--kl_coeff", type=float, default=0.1)
+    parser.add_argument("--latent_dim", type=int, default=128)
     # use analytic KL
-    parser.add_argument('--analytic', type=int, default=0)
+    parser.add_argument("--analytic", type=int, default=0)
 
     # number of samples to use for validation
-    parser.add_argument('--val_samples', type=int, default=1)
+    parser.add_argument("--val_samples", type=int, default=1)
 
     # optimizer param
-    parser.add_argument('--learning_rate', type=float, default=1e-3)
-    parser.add_argument('--warmup_epochs', type=int, default=10)
-    parser.add_argument('--max_epochs', type=int, default=800)
+    parser.add_argument("--learning_rate", type=float, default=1e-3)
+    parser.add_argument("--warmup_epochs", type=int, default=10)
+    parser.add_argument("--max_epochs", type=int, default=800)
     parser.add_argument("--cosine_decay", type=int, default=0)
     parser.add_argument("--linear_decay", type=int, default=0)
 
     # training params
-    parser.add_argument('--gpus', type=int, default=1)
-    parser.add_argument("--fp16", action='store_true')
+    parser.add_argument("--gpus", type=int, default=1)
+    parser.add_argument("--fp16", action="store_true")
 
     # datamodule params
-    parser.add_argument('--data_path', type=str, default='.')
+    parser.add_argument("--data_path", type=str, default=".")
     parser.add_argument(
-        '--dataset', type=str, default="cifar10"
+        "--dataset", type=str, default="cifar10"
     )  # cifar10, stl10, imagenet
-    parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--num_workers", type=int, default=8)
 
     # transforms param
-    parser.add_argument('--input_height', type=int, default=32)
-    parser.add_argument('--gaussian_blur', type=bool, default=True)
-    parser.add_argument('--jitter_strength', type=float, default=1.0)
+    parser.add_argument("--input_height", type=int, default=32)
+    parser.add_argument("--gaussian_blur", type=bool, default=True)
+    parser.add_argument("--jitter_strength", type=float, default=1.0)
 
     args = parser.parse_args()
 
     # set hidden dim for resnet18
-    if args.encoder == 'resnet18':
+    if args.encoder == "resnet18":
         args.h_dim = 512
 
-    if args.dataset == 'cifar10':
+    if args.dataset == "cifar10":
         dm = CIFAR10DataModule(
             data_dir=args.data_path,
             batch_size=args.batch_size,
@@ -382,7 +387,7 @@ if __name__ == "__main__":
 
         args.gaussian_blur = False
         args.jitter_strength = 0.5
-    elif args.dataset == 'stl10':
+    elif args.dataset == "stl10":
         dm = STL10DataModule(
             data_dir=args.data_path,
             batch_size=args.batch_size,
@@ -397,7 +402,7 @@ if __name__ == "__main__":
         args.maxpool1 = False
         args.first_conv = True
         normalization = stl10_normalization()
-    elif args.dataset == 'imagenet':
+    elif args.dataset == "imagenet":
         dm = ImagenetDataModule(
             data_dir=args.data_path,
             batch_size=args.batch_size,
@@ -439,12 +444,12 @@ if __name__ == "__main__":
         interpolate_epoch_interval=20, range_start=-3, range_end=3, normalize=True
     )
 
-    lr_monitor = LearningRateMonitor(logging_interval='step')
+    lr_monitor = LearningRateMonitor(logging_interval="step")
 
     trainer = pl.Trainer(
         max_epochs=args.max_epochs,
         gpus=args.gpus,
-        distributed_backend='ddp' if args.gpus > 1 else None,
+        distributed_backend="ddp" if args.gpus > 1 else None,
         precision=16 if args.fp16 else 32,
         callbacks=[online_evaluator, interpolator, lr_monitor],
     )
