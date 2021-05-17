67cd7b382583fc503ddf8bea895a0966db6d8033
ananyahjha93 <ananya@pytorchlightning.ai>
2021-05-17 03:55:10 -0400

    sort uniform sampled array

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+# can set min, max and n
 LOG_SCALE=$(awk -v n=8 -v min=-5 -v max=2 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) print rand() * (max - min) + min }')
-echo $(LOG_SCALE[*])
-# LR=1e-4
-# KL=1e-3
+LOG_SCALE=($(echo ${LOG_SCALE=[*]}| tr " " "\n" | sort -n))
+LR=1e-4
+KL=1e-3
 
-# for i in {0..8}; do
-#     screen -dmS "run$i" bash -c "source /home/ananya/env/bin/activate; python setup.py install; CUDA_VISIBLE_DEVICES=$i python src/vae.py --seed $(date +%s) --denoising --gpus 1 --batch_size 256 --warmup_epochs 10 --val_samples 16 --weight_decay 0 --learning_rate $LR --kl_coeff $KL --log_scale ${LOG_SCALE[$i]}; exec sh"
-#     sleep 5
-# done
+for i in {0..8}; do
+    screen -dmS "run$i" bash -c "source /home/ananya/env/bin/activate; python setup.py install; CUDA_VISIBLE_DEVICES=$i python src/vae.py --seed $(date +%s) --denoising --gpus 1 --batch_size 256 --warmup_epochs 10 --val_samples 16 --weight_decay 0 --learning_rate $LR --kl_coeff $KL --log_scale ${LOG_SCALE[$i]}; exec sh"
+    sleep 5
+done
