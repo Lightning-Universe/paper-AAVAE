c0dba53626f90d07ff10202ecdef0d64fc0aec0b
ananyahjha93 <ananya@pytorchlightning.ai>
2021-05-17 04:41:50 -0400

    tensorboard name

--- tensorboard.sh
+++ tensorboard.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-NAME="cifar10-kl-1e-3-lr-1e-4-wd-sweep"
+NAME="cifar10-lr-1e-4-kl-1e-3-logscale-sweep"
 
 screen -dmS "logging" bash -c "source /home/ananya/env/bin/activate; tensorboard dev upload --logdir lightning_logs/ --name $NAME; exec sh"
