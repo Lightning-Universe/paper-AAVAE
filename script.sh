5f896f00e4df84ddbc17a95669d5c0fd9d94bf86
ananyahjha93 <ananya@pytorchlightning.ai>
2021-05-17 04:28:06 -0400

    updated script

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ LOG_SCALE=($(echo ${LOG_SCALE=[*]}| tr " " "\n" | sort -n))
 LR=1e-4
 KL=1e-3
 
-for i in {0..8}; do
+for i in {0..7}; do
     screen -dmS "run$i" bash -c "source /home/ananya/env/bin/activate; python setup.py install; CUDA_VISIBLE_DEVICES=$i python src/vae.py --seed $(date +%s) --denoising --gpus 1 --batch_size 256 --warmup_epochs 10 --val_samples 16 --weight_decay 0 --learning_rate $LR --kl_coeff $KL --log_scale ${LOG_SCALE[$i]}; exec sh"
     sleep 5
 done
