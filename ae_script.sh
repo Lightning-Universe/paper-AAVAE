9b28c5fa18ad705e44936d77c3118d828d8c5fb6
ananyahjha93 <ananya@pytorchlightning.ai>
2021-05-30 12:09:36 -0400

    max epochs

--- ae_script.sh
+++ ae_script.sh
@@ -7,6 +7,6 @@ BATCH_SIZE=256
 WARMUP_EPOCHS=10
 
 for i in {0..7}; do
-    screen -dmS "run$i" bash -c "source /home/ananya/env/bin/activate; python setup.py install; CUDA_VISIBLE_DEVICES=$i python src/ae.py --seed $(date +%s) --online_ft --denoising --gpus 1 --batch_size $BATCH_SIZE --warmup_epochs $WARMUP_EPOCHS --learning_rate $LR --weight_decay ${WEIGHT_DECAY[$i]}; exec sh"
+    screen -dmS "run$i" bash -c "source /home/ananya/env/bin/activate; python setup.py install; CUDA_VISIBLE_DEVICES=$i python src/ae.py --seed $(date +%s) --online_ft --denoising --gpus 1 --max_epochs 5000 --batch_size $BATCH_SIZE --warmup_epochs $WARMUP_EPOCHS --learning_rate $LR --weight_decay ${WEIGHT_DECAY[$i]}; exec sh"
     sleep 5
 done
